e65a4f6e2f82d496ae70c0b425ff48d0afea7915
Romain GALLAND <romain.galland@rgld.fr>
2022-02-24 17:16:38 +0100

    adding rich support for better comprehension of the text.

--- main.py
+++ main.py
@@ -1,31 +1,51 @@
 from pynput.keyboard import Key, Controller, KeyCode
 import time
+from rich.progress import Progress
+from rich import print
 
-if __name__ == '__main__':
-    t = str(input("Saisissez le temps avant d'éteindre la musique: (ex: 20m)\n\t> "))
-    minutes = 0
-    hour = 0
-    t_ = t.split("h")
-    if len(t_) == 2:
-        minutes = int(t_[1].replace("m", ""))
-        hour = int(t_[0])
-    elif len(t_) == 1:
-        if "m" in t:
-            minutes = int(t_[0].replace("m", ""))
-        elif "h" in t:
-            hour = int(t_[0])
-    print(f"Sleep in {minutes} minutes and {hour} hours.")
+
+def get_time():
+    minutes_ = input("Minutes ? \n\t> ")
+    if minutes_ == "":
+        minutes = 0
+    else:
+        minutes = int(minutes_)
+
+    hours_ = input("Hours ? \n\t> ")
+    if hours_ == "":
+        hours = 0
+    else:
+        hours = int(hours_)
+
+    return minutes, hours
+
+
+if __name__ == "__main__":
+    minutes, hours = get_time()
+
+    secs = int(60 * (minutes + 60 * hours))
+    while secs == 0:
+        print("[red]:warning: The cooldown can't be null !")
+        minutes, hours = get_time()
+        secs = int(60 * (minutes + 60 * hours))
+
+    print(f"[red]Sleep in [b]{hours} hours[/b] and [b]{minutes} minutes[/b]")
     keyboard = Controller()
-    secs = 60*minutes + 60*60*hour
-    for i in range(secs):
-        time.sleep(0.9)
-        print(secs-i)
-    for i in range(100):
-        keyboard.press(Key.media_volume_down)
-        keyboard.release(Key.media_volume_down)
-        time.sleep(0.5)
-    print("Music Paused")
+    with Progress() as pr:
+        task1 = pr.add_task("[blue] Cooldown...", total=secs)
+        while not pr.finished:
+            pr.update(task1, advance=1)
+            time.sleep(1)
+
+    with Progress() as pr:
+        task2 = pr.add_task("[gray italic] Sleep...", total=100)
+        while not pr.finished:
+            pr.update(task2, advance=1)
+            keyboard.press(Key.media_volume_down)
+            keyboard.release(Key.media_volume_down)
+            time.sleep(0.5)
     keyboard.press(KeyCode.from_vk(0xB3))
+    print("[green b u]Music Paused")
     for i in range(100):
         keyboard.press(Key.media_volume_up)
         keyboard.release(Key.media_volume_up)
